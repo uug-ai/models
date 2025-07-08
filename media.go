8281702b2ec75fb4ea5b1cb7999372c7c6ed1e9e
cedricve <cedric@verstraeten.io>
2025-07-08 09:20:02 +0000

    Define Media struct with additional fields and metadata; add Dockerfile and devcontainer.json for development setup

--- media.go
+++ media.go
@@ -1,17 +1,21 @@
 package main
 
 type Media struct {
-	FileName       string `json:"fileName,omitempty" bson:"fileName,omitempty"`
-	StartTimestamp int64  `json:"startTimestamp,omitempty" bson:"startTimestamp,omitempty"`
-	EndTimestamp   int64  `json:"endTimestamp,omitempty" bson:"endTimestamp,omitempty"`
-	Duration       int64  `json:"duration,omitempty" bson:"duration,omitempty"`
-	Provider       string `json:"provider,omitempty" bson:"provider,omitempty"`
-	Storage        string `json:"storage,omitempty" bson:"storage,omitempty"`
-	DeviceId       string `json:"deviceId,omitempty" bson:"deviceId,omitempty"`
-	GroupId        string `json:"groupId,omitempty" bson:"groupId,omitempty"`
-	UserId         string `json:"userId,omitempty" bson:"userId,omitempty"`
+	// Time window of media file.
+	StartTimestamp int64 `json:"startTimestamp,omitempty" bson:"startTimestamp,omitempty"`
+	EndTimestamp   int64 `json:"endTimestamp,omitempty" bson:"endTimestamp,omitempty"`
+	Duration       int64 `json:"duration,omitempty" bson:"duration,omitempty"`
 
+	// Media file owner
+	DeviceId string `json:"deviceId,omitempty" bson:"deviceId,omitempty"`
+	GroupId  string `json:"groupId,omitempty" bson:"groupId,omitempty"`
+	UserId   string `json:"userId,omitempty" bson:"userId,omitempty"`
+
+	// Media file information
+	Storage           string `json:"storage,omitempty" bson:"storage,omitempty"`
+	VideoFile         string `json:"videoFile,omitempty" bson:"videoFile,omitempty"`
 	VideoUrl          string `json:"video_url,omitempty" bson:"video_url,omitempty"`
+	VideoProvider     string `json:"videoProvider,omitempty" bson:"videoProvider,omitempty"`
 	ThumbnailUrl      string `json:"thumbnail_url,omitempty" bson:"thumbnail_url,omitempty"`
 	ThumbnailFile     string `json:"thumbnailFile,omitempty" bson:"thumbnailFile,omitempty"`
 	ThumbnailProvider string `json:"thumbnailProvider,omitempty" bson:"thumbnailProvider,omitempty"`
@@ -19,4 +23,12 @@ type Media struct {
 	SpriteFile        string `json:"spriteFile,omitempty" bson:"spriteFile,omitempty"`
 	SpriteProvider    string `json:"spriteProvider,omitempty" bson:"spriteProvider,omitempty"`
 	SpriteInterval    int    `json:"spriteInterval,omitempty" bson:"spriteInterval,omitempty"`
+
+	// Metadata
+	Metadata []MediaMetadata `json:"metadata,omitempty" bson:"metadata,omitempty"`
+}
+
+type MediaMetadata struct {
+	Tags            []string `json:"tags,omitempty" bson:"tags,omitempty"`
+	Classifications []string `json:"classifications,omitempty" bson:"classifications,omitempty"`
 }
