2831c2cbfad0b155cf4249f004eb0f4260984015
cedricve <cedric@verstraeten.io>
2025-07-11 20:40:55 +0000

    Refactor: update Site struct to include RBAC fields and enhance metadata handling; remove accidental Username field

--- pkg/models/sites.go
+++ pkg/models/sites.go
@@ -5,28 +5,37 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Site struct {
 	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty,omitempty"`
 	Name        string             `json:"name" bson:"name,omitempty"`
-	Username    string             `json:"username" bson:"username,omitempty"` // added by accident should be removed!
 	Description string             `json:"description" bson:"description,omitempty"`
-	Initials    string             `json:"initials" bson:"initials,omitempty"`
-	Color       string             `json:"color" bson:"color,omitempty"`
 
-	UserId     string   `json:"user_id" bson:"user_id,omitempty"`
-	AllDevices []string `json:"all_devices" bson:"all_devices"` // Calculated on the fly!
-	Devices    []string `json:"devices" bson:"devices"`
-	Groups     []string `json:"groups" bson:"groups"`
+	// RBAC information
+	OrganisationId string   `json:"organisationId" bson:"organisationId,omitempty"`
+	Devices        []string `json:"devices" bson:"devices"`
+	Groups         []string `json:"groups" bson:"groups"`
 
-	AccessKey  string `json:"access_key" bson:"access_key"`
-	SecretKey  string `json:"secret_key" bson:"secret_key"`
-	StorageUri string `json:"storage_uri" bson:"storage_uri"`
+	// Media file information (by default "vault", however might change
+	// in the future (integration with other storage solutions, next to Vault).
+	StorageSolution string `json:"storageSolution,omitempty" bson:"storageSolution,omitempty"`
 
-	FloorPlans         []FloorPlan `json:"floor_plans" bson:"floor_plans,omitempty"`
-	NumberOfFloorPlans int         `json:"numberOfFloorPlans" bson:"numberOfFloorPlans,omitempty"`
+	VaultAccessKey string `json:"vaultAccessKey" bson:"vaultAccessKey"`
+	VaultSecretKey string `json:"vaultSecretKey" bson:"vaultSecretKey"`
+	VaultUri       string `json:"vaultUri" bson:"vaultUri"`
+
+	// Metadata
+	Metadata *SiteMetadata `json:"metadata,omitempty" bson:"metadata,omitempty"`
 
 	// Location metadata
 	LocationMetadata *SiteLocationMetadata `json:"locationMetadata,omitempty" bson:"locationMetadata,omitempty"`
 
 	// Audit information
 	Audit *Audit `json:"audit,omitempty" bson:"audit,omitempty"`
+}
+
+// SiteMetadata contains additional metadata for the site, such as tags and classifications.
+type SiteMetadata struct {
+	Initials           string      `json:"initials" bson:"initials,omitempty"`
+	Color              string      `json:"color" bson:"color,omitempty"`
+	FloorPlans         []FloorPlan `json:"floorPlans" bson:"floorPlans,omitempty"` // List of floor plans associated with the site
+	NumberOfFloorPlans int         `json:"numberOfFloorPlans" bson:"numberOfFloorPlans,omitempty"`
 }
 
 // SiteLocationMetadata contains metadata about the physical location of the site.
