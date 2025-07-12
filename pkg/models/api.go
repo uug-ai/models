2e34bdf4cf62a75155ddd60e2febb1fac8ed0d34
cedricve <cedric@verstraeten.io>
2025-07-12 06:53:55 +0000

    Refactor: standardize JSON field names in APIMetadata, ErrorResponse, and SuccessResponse structs

--- pkg/models/api.go
+++ pkg/models/api.go
@@ -16,42 +16,30 @@ const (
 )
 
 type APIMetadata struct {
-	UserId    string `json:"user_id,omitempty" bson:"user_id,omitempty"`
-	Timestamp int64  `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
-	Error     error  `json:"error,omitempty" bson:"error,omitempty"`
-	Path      string `json:"path,omitempty" bson:"path,omitempty"`
+	TraceId        string `json:"traceId,omitempty" bson:"traceId,omitempty"`
+	OrganisationId string `json:"organisationId,omitempty" bson:"organisationId,omitempty"`
+	UserId         string `json:"userId,omitempty" bson:"userId,omitempty"`
+	Timestamp      int64  `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
+	Error          error  `json:"error,omitempty" bson:"error,omitempty"`
+	Path           string `json:"path,omitempty" bson:"path,omitempty"`
 }
 
 type ErrorResponse struct {
-	StatusCode int         `json:"status_code,omitempty" bson:"status_code,omitempty"`
-	RequestId  string      `json:"request_id,omitempty" bson:"request_id,omitempty"`
-	MetaData   APIMetadata `json:"meta_data,omitempty" bson:"meta_data,omitempty"`
-	ErrorCode  string      `json:"error_code,omitempty" bson:"error_code,omitempty"` // More specific custom error code or type
+	StatusCode int         `json:"statusCode,omitempty" bson:"statusCode,omitempty"`
+	RequestId  string      `json:"requestId,omitempty" bson:"requestId,omitempty"`
+	MetaData   APIMetadata `json:"metaData,omitempty" bson:"metaData,omitempty"`
+	ErrorCode  string      `json:"errorCode,omitempty" bson:"errorCode,omitempty"` // More specific custom error code or type
 	Message    string      `json:"message,omitempty" bson:"message,omitempty"`
 }
 
 type SuccessResponse struct {
-	StatusCode  int         `json:"status_code,omitempty" bson:"status_code,omitempty"`
-	RequestId   string      `json:"request_id,omitempty" bson:"request_id,omitempty"`
-	MetaData    APIMetadata `json:"meta_data,omitempty" bson:"meta_data,omitempty"`
-	SuccessCode string      `json:"success_code,omitempty" bson:"success_code,omitempty"`
+	StatusCode  int         `json:"statusCode,omitempty" bson:"statusCode,omitempty"`
+	RequestId   string      `json:"requestId,omitempty" bson:"requestId,omitempty"`
+	MetaData    APIMetadata `json:"metaData,omitempty" bson:"metaData,omitempty"`
+	SuccessCode string      `json:"successCode,omitempty" bson:"successCode,omitempty"`
 	Message     string      `json:"message,omitempty" bson:"message,omitempty"`
 }
 
-// Example of Error reponse
-//
-//	{
-//		"statuscode": 400,
-//		"requestId": "123456",
-//		"meta_data": {
-//			"user_id": "123456",
-//			"timestamp": 1234567890,
-//			"error": "error"
-//			"path": "/api/v1/xxx"
-//		},
-//		"errorCode": "error_code",
-//		"message": "Error message"
-//	}
 func CreateError(statusCode int, errorCode string, message string, metadata APIMetadata) ErrorResponse {
 	metadata.Timestamp = time.Now().Unix()
 	return ErrorResponse{
@@ -62,21 +50,6 @@ func CreateError(statusCode int, errorCode string, message string, metadata APIM
 	}
 }
 
-// Example of Success reponse
-//
-//	{
-//			"statuscode": 200,
-//			"requestId": "123456",
-//			"meta_data": {
-//				"user_id": "123456",
-//				"timestamp": 1234567890,
-//				"error": "error"
-//				"path": "/api/v1/xxx"
-//			},
-//			"successCode": "success_code",
-//			"message": "Success message",
-//			"data": {}
-//		}
 func CreateSuccess(statusCode int, successCode string, message string, metadata APIMetadata) SuccessResponse {
 	metadata.Timestamp = time.Now().Unix()
 	return SuccessResponse{
