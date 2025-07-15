1757ebfe481422e98988ac3f3edbde698f6e98ef
cedricve <cedric@verstraeten.io>
2025-07-15 13:49:35 +0000

    Refactor: restructure Synchronize and SynchronizeEvent types for improved clarity and organization

--- pkg/models/synchronize.go
+++ pkg/models/synchronize.go
@@ -11,12 +11,14 @@ const (
 )
 
 type Synchronize struct {
-	Status   Status              `json:"status,omitempty" bson:"status,omitempty"`     // Status of synchronization with external systems
-	WorkerId string              `json:"workerId,omitempty" bson:"workerId,omitempty"` // ID of the worker handling synchronization
-	Events   []SynchronizeEvents `json:"events,omitempty" bson:"events,omitempty"`     // Events related to synchronization, such as timestamps and messages
+	SynchronizeEvent                    // This is the current event of synchronization
+	Events           []SynchronizeEvent `json:"events,omitempty" bson:"events,omitempty"` // History of synchronization events
 }
 
-type SynchronizeEvents struct {
+type SynchronizeEvent struct {
 	Timestamp int64  `json:"timestamp,omitempty" bson:"timestamp,omitempty"` // Timestamp of the last synchronization attempt
+	Status    Status `json:"status,omitempty" bson:"status,omitempty"`       // Status of synchronization with external systems
+	WorkerId  string `json:"workerId,omitempty" bson:"workerId,omitempty"`   // ID of the worker handling synchronization
 	Message   string `json:"message,omitempty" bson:"message,omitempty"`     // Additional message or error description related to synchronization
+
 }
