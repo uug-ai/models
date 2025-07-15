af574720cb8036b7c141f08e9ae2ae9e19b17812
cedricve <cedric@verstraeten.io>
2025-07-15 22:17:07 +0000

    Refactor: remove duplicate Timestamp field and Message field from Metadata struct for clarity

--- pkg/api/metadata.go
+++ pkg/api/metadata.go
@@ -1,10 +1,9 @@
 package api
 
 type Metadata struct {
+	Timestamp      int64  `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
 	TraceId        string `json:"traceId,omitempty" bson:"traceId,omitempty"`
 	OrganisationId string `json:"organisationId,omitempty" bson:"organisationId,omitempty"`
 	UserId         string `json:"userId,omitempty" bson:"userId,omitempty"`
-	Timestamp      int64  `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
 	Path           string `json:"path,omitempty" bson:"path,omitempty"`
-	Message        string `json:"message,omitempty" bson:"message,omitempty"`
 }
