e0680e1ecce5688370a92bc46b6cba3c70e8fb5b
cedricve <cedric@verstraeten.io>
2025-07-15 13:43:28 +0000

    Refactor: rename synchronization status constants for consistency and update struct formatting

--- pkg/models/synchronize.go
+++ pkg/models/synchronize.go
@@ -3,16 +3,20 @@ package models
 type Status string
 
 const (
-	SynchronizationStatusPending    Status = "pending"
-	SynchronizationStatusInProgress Status = "inProgress"
-	SynchronizationStatusSkipped    Status = "skipped"
-	SynchronizationStatusCompleted  Status = "completed"
-	SynchronizationStatusFailed     Status = "failed"
+	SynchronizeStatusPending    Status = "pending"
+	SynchronizeStatusInProgress Status = "inProgress"
+	SynchronizeStatusSkipped    Status = "skipped"
+	SynchronizeStatusCompleted  Status = "completed"
+	SynchronizeStatusFailed     Status = "failed"
 )
 
 type Synchronize struct {
-	Status    Status `json:"status,omitempty" bson:"status,omitempty"`       // Status of synchronization with external systems
-	WorkerId  string `json:"workerId,omitempty" bson:"workerId,omitempty"`   // ID of the worker handling synchronization
+	Status   Status              `json:"status,omitempty" bson:"status,omitempty"`     // Status of synchronization with external systems
+	WorkerId string              `json:"workerId,omitempty" bson:"workerId,omitempty"` // ID of the worker handling synchronization
+	Events   []SynchronizeEvents `json:"events,omitempty" bson:"events,omitempty"`     // Events related to synchronization, such as timestamps and messages
+}
+
+type SynchronizeEvents struct {
 	Timestamp int64  `json:"timestamp,omitempty" bson:"timestamp,omitempty"` // Timestamp of the last synchronization attempt
 	Message   string `json:"message,omitempty" bson:"message,omitempty"`     // Additional message or error description related to synchronization
 }
