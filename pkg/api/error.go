8c11cd7dea96bb40cf25721edf342566804c83a4
cedricve <cedric@verstraeten.io>
2025-07-15 22:16:13 +0000

    Refactor: reorder MetaData field in ErrorResponse and SuccessResponse structs for consistency

--- pkg/api/error.go
+++ pkg/api/error.go
@@ -4,19 +4,18 @@ import "time"
 
 type ErrorResponse struct {
 	StatusCode int      `json:"statusCode,omitempty" bson:"statusCode,omitempty"`
-	RequestId  string   `json:"requestId,omitempty" bson:"requestId,omitempty"`
-	MetaData   Metadata `json:"metaData,omitempty" bson:"metaData,omitempty"`
 	ErrorCode  int      `json:"errorCode,omitempty" bson:"errorCode,omitempty"` // More specific custom error code or type
 	Error      error    `json:"error,omitempty" bson:"error,omitempty"`
 	Message    string   `json:"message,omitempty" bson:"message,omitempty"`
+	MetaData   Metadata `json:"metaData,omitempty" bson:"metaData,omitempty"`
 }
 
 func CreateError(statusCode int, errorCode int, message string, metadata Metadata) ErrorResponse {
 	metadata.Timestamp = time.Now().Unix()
 	return ErrorResponse{
 		StatusCode: statusCode,
-		MetaData:   metadata,
 		ErrorCode:  errorCode,
 		Message:    message,
+		MetaData:   metadata,
 	}
 }
--- pkg/api/success.go
+++ pkg/api/success.go
@@ -4,18 +4,17 @@ import "time"
 
 type SuccessResponse struct {
 	StatusCode  int      `json:"statusCode,omitempty" bson:"statusCode,omitempty"`
-	RequestId   string   `json:"requestId,omitempty" bson:"requestId,omitempty"`
-	MetaData    Metadata `json:"metaData,omitempty" bson:"metaData,omitempty"`
 	SuccessCode int      `json:"successCode,omitempty" bson:"successCode,omitempty"`
 	Message     string   `json:"message,omitempty" bson:"message,omitempty"`
+	MetaData    Metadata `json:"metaData,omitempty" bson:"metaData,omitempty"`
 }
 
 func CreateSuccess(statusCode int, successCode int, message string, metadata Metadata) SuccessResponse {
 	metadata.Timestamp = time.Now().Unix()
 	return SuccessResponse{
 		StatusCode:  statusCode,
-		MetaData:    metadata,
 		SuccessCode: successCode,
 		Message:     message,
+		MetaData:    metadata,
 	}
 }
