4c47f0f446ab1aca93bff57cb23fa96ee2817918
Kilian <kilian.boute@gmail.com>
2025-09-18 07:21:01 +0000

    Add AccessTokenScope type and update Scopes field in AccessToken struct

--- pkg/models/accesstoken.go
+++ pkg/models/accesstoken.go
@@ -1,11 +1,18 @@
 package models
 
+type AccessTokenScope string
+
+const (
+	MarkersWrite AccessTokenScope = "markers:write"
+	MarkersRead  AccessTokenScope = "markers:read"
+)
+
 type AccessToken struct {
-	Name        string   `json:"name" bson:"name,omitempty"`
-	Description string   `json:"description,omitempty" bson:"description,omitempty"` // Description of the access token
-	Expiration  int64    `json:"expiration,omitempty" bson:"expiration,omitempty"`   // Expiration timestamp of the access token in seconds since epoch
-	Scopes      []string `json:"scopes,omitempty" bson:"scopes,omitempty"`           // List of scopes associated with the access token
-	Token       string   `json:"token,omitempty" bson:"token,omitempty"`             // The actual access token value, should be kept secret (will be trimmed before saving)
+	Name        string             `json:"name" bson:"name,omitempty"`
+	Description string             `json:"description,omitempty" bson:"description,omitempty"` // Description of the access token
+	Expiration  int64              `json:"expiration,omitempty" bson:"expiration,omitempty"`   // Expiration timestamp of the access token in seconds since epoch
+	Scopes      []AccessTokenScope `json:"scopes,omitempty" bson:"scopes,omitempty"`           // List of scopes associated with the access token
+	Token       string             `json:"token,omitempty" bson:"token,omitempty"`             // The actual access token value, should be kept secret (will be trimmed before saving)
 
 	// RBAC information
 	OrganisationId string `json:"organisationId" bson:"organisationId,omitempty"` // OrganisationId is used to identify the organisation that owns the marker
