371e192859f57bc025ef7911c44d28c6258d70ee
Cédric Verstraeten <cedric@verstraeten.io>
2026-01-20 09:42:56 +0000

    feat(models): add StateEnum type and update State struct to use it for desired state

--- pkg/models/state.go
+++ pkg/models/state.go
@@ -5,6 +5,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// State enumeration
+type StateEnum string
+
+const (
+	StateActive      StateEnum = "active"
+	StatePaused      StateEnum = "paused"
+	StateNoRecording StateEnum = "no_recording"
+	StateNoLiveView  StateEnum = "no_live_view"
+)
+
 // State represents the state pattern used for a specific device.
 // This allows to achieve specific behaviors or configurations based on the defined state.
 type State struct {
@@ -12,7 +22,7 @@ type State struct {
 	Name        string             `json:"name" bson:"name" example:"Default State" required:"true"`                                   // Name of the state
 	Description string             `json:"description,omitempty" bson:"description,omitempty" example:"Person forcably opened a door"` // Description of the status
 
-	// Resource identification
+	// Resource identification, to which this state applies
 	DeviceId       string   `json:"deviceId" bson:"deviceId" example:"686a906345c1df594939f9j25f4"`                                                         // DeviceId is used to identify the device associated with the state
 	SiteId         string   `json:"siteId,omitempty" bson:"siteId,omitempty" example:"686a906345c1df594pcsr3r45"`                                           // SiteId is used to identify the site for which the state is relevant
 	GroupId        string   `json:"groupId,omitempty" bson:"groupId,omitempty" example:"686a906345c1df594pmt41w4"`                                          // GroupId is used to identify the group for which the state is relevant
@@ -20,10 +30,13 @@ type State struct {
 	Devices        []string `json:"devices,omitempty" bson:"devices,omitempty" example:"[\"686a906345c1df594939f9j25f4\",\"686a906345c1df594939f9j25f5\"]"` // List of device IDs associated with the state
 
 	// Timing information (all timestamps are in seconds)
-	StartTimestamp int64  `json:"startTimestamp" bson:"startTimestamp" example:"1752482068" required:"true"` // Start timestamp of the marker in seconds since epoch
-	EndTimestamp   int64  `json:"endTimestamp" bson:"endTimestamp" example:"1752482079" required:"true"`     // End timestamp of the marker in seconds since epoch
-	Duration       int    `json:"duration" bson:"duration" example:"11" required:"true"`                     // Duration of the state in seconds
-	State          string `json:"state" bson:"state" example:"active" required:"true"`                       // Current state, e.g., "active", "inactive"
+	DesiredState StateEnum `json:"state" bson:"state" example:"active" required:"true"` // Desired state to be applied to the device, if all conditions are met.
+
+	// Conditions for the state to be applied
+	// @To be implemented in future releases
+	// Conditions Conditions `json:"conditions,omitempty" bson:"conditions,omitempty"` // Conditions that trigger the state change (to be implemented in future releases)
+	StartTimestamp int64 `json:"startTimestamp" bson:"startTimestamp" example:"1752482068" required:"true"` // Start timestamp of the marker in seconds since epoch
+	EndTimestamp   int64 `json:"endTimestamp" bson:"endTimestamp" example:"1752482079" required:"true"`     // End timestamp of the marker in seconds since epoch
 
 	// Additional metadata
 	Metadata *StateMetadata `json:"metadata,omitempty" bson:"metadata,omitempty"` // Metadata associated with the marker, such as comments and tags
