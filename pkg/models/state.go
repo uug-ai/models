5314d0d0085909bb3b2c8b262944188f617007ea
Cédric Verstraeten <cedric@verstraeten.io>
2026-01-20 09:35:29 +0000

    fix(models): correct struct field annotations and formatting in State model

--- pkg/models/state.go
+++ pkg/models/state.go
@@ -1,24 +1,29 @@
 package models
 
+import (
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 // State represents the state pattern used for a specific device.
 // This allows to achieve specific behaviors or configurations based on the defined state.
 type State struct {
-	Id                  primitive.ObjectID `json:"id" bson:"_id,omitempty"` `json:"id" bson:"_id" example:"state-123" required:"true"`                                       // Unique identifier for the state
-	Name        string `json:"name" bson:"name" example:"Default State" required:"true"`                                // Name of the state
-	Description string `json:"description,omitempty" bson:"description,omitempty" example:"Person forcably opened a door"` // Description of the status
+	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`                                                                    // Unique identifier for the state
+	Name        string             `json:"name" bson:"name" example:"Default State" required:"true"`                                   // Name of the state
+	Description string             `json:"description,omitempty" bson:"description,omitempty" example:"Person forcably opened a door"` // Description of the status
 
 	// Resource identification
-	DeviceId       string `json:"deviceId" bson:"deviceId" example:"686a906345c1df594939f9j25f4"` // DeviceId is used to identify the device associated with the state
-	SiteId         string `json:"siteId,omitempty" bson:"siteId,omitempty" example:"686a906345c1df594pcsr3r45"`   // SiteId is used to identify the site for which the state is relevant
-	GroupId        string `json:"groupId,omitempty" bson:"groupId,omitempty" example:"686a906345c1df594pmt41w4"`  // GroupId is used to identify the group for which the state is relevant
-	OrganisationId string `json:"organisationId" bson:"organisationId" example:"686a906345c1df594pad69f0"`        // OrganisationId is used to identify the organisation that owns the state.
+	DeviceId       string   `json:"deviceId" bson:"deviceId" example:"686a906345c1df594939f9j25f4"`                                                         // DeviceId is used to identify the device associated with the state
+	SiteId         string   `json:"siteId,omitempty" bson:"siteId,omitempty" example:"686a906345c1df594pcsr3r45"`                                           // SiteId is used to identify the site for which the state is relevant
+	GroupId        string   `json:"groupId,omitempty" bson:"groupId,omitempty" example:"686a906345c1df594pmt41w4"`                                          // GroupId is used to identify the group for which the state is relevant
+	OrganisationId string   `json:"organisationId" bson:"organisationId" example:"686a906345c1df594pad69f0"`                                                // OrganisationId is used to identify the organisation that owns the state.
 	Devices        []string `json:"devices,omitempty" bson:"devices,omitempty" example:"[\"686a906345c1df594939f9j25f4\",\"686a906345c1df594939f9j25f5\"]"` // List of device IDs associated with the state
-	
+
 	// Timing information (all timestamps are in seconds)
-	StartTimestamp int64 `json:"startTimestamp" bson:"startTimestamp" example:"1752482068" required:"true"` // Start timestamp of the marker in seconds since epoch
-	EndTimestamp   int64 `json:"endTimestamp" bson:"endTimestamp" example:"1752482079" required:"true"`     // End timestamp of the marker in seconds since epoch
-	Duration       int   `json:"duration" bson:"duration" example:"11" required:"true"`                     // Duration of the state in seconds
-	State 		   string `json:"state" bson:"state" example:"active" required:"true"`                     // Current state, e.g., "active", "inactive"
+	StartTimestamp int64  `json:"startTimestamp" bson:"startTimestamp" example:"1752482068" required:"true"` // Start timestamp of the marker in seconds since epoch
+	EndTimestamp   int64  `json:"endTimestamp" bson:"endTimestamp" example:"1752482079" required:"true"`     // End timestamp of the marker in seconds since epoch
+	Duration       int    `json:"duration" bson:"duration" example:"11" required:"true"`                     // Duration of the state in seconds
+	State          string `json:"state" bson:"state" example:"active" required:"true"`                       // Current state, e.g., "active", "inactive"
 
 	// Additional metadata
 	Metadata *StateMetadata `json:"metadata,omitempty" bson:"metadata,omitempty"` // Metadata associated with the marker, such as comments and tags
