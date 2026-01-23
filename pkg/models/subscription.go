8bccec2f437f2cdb516c7e1b1547ab452dc46816
Cédric Verstraeten <cedric@verstraeten.io>
2026-01-23 13:23:33 +0000

    Refactor Subscription struct and add property constants for BSON fields

--- pkg/models/subscription.go
+++ pkg/models/subscription.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Subscription struct {
-	Id     primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Name   string             `json:"name,omitempty"`
-	UserId string             `json:"user_id" bson:"user_id,omitempty"`
-	Plan   string             `json:"stripe_plan" bson:"stripe_plan,omitempty"`
-	Ends   string             `json:"ends_at" bson:"ends_at,omitempty"`
+	Id         primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name       string             `json:"name,omitempty"`
+	UserId     string             `json:"user_id" bson:"user_id,omitempty"`
+	StripePlan string             `json:"stripe_plan" bson:"stripe_plan,omitempty"`
+	EndsAt     string             `json:"ends_at" bson:"ends_at,omitempty"`
 }
---
+++ pkg/properties/subscription.go
@@ -0,0 +1,10 @@
+package properties
+
+// Subscription property field names (BSON)
+const (
+	SubscriptionId         = "_id"
+	SubscriptionName       = "name"
+	SubscriptionUserId     = "user_id"
+	SubscriptionStripePlan = "stripe_plan"
+	SubscriptionEndsAt     = "ends_at"
+)
