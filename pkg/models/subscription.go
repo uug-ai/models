6048af66887da20ae7cb8eb31724b05bbb800355
cedricve <cedric@verstraeten.io>
2026-01-24 21:01:09 +0000

    Refactor Subscription struct to include additional fields for enhanced BSON representation

--- pkg/models/subscription.go
+++ pkg/models/subscription.go
@@ -5,9 +5,13 @@ import (
 )
 
 type Subscription struct {
-	Id         primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Name       string             `json:"name,omitempty"`
-	UserId     string             `json:"user_id" bson:"user_id,omitempty"`
-	StripePlan string             `json:"stripe_plan" bson:"stripe_plan,omitempty"`
-	EndsAt     string             `json:"ends_at" bson:"ends_at,omitempty"`
+	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	StripeId    string             `json:"stripe_id,omitempty" bson:"stripe_id,omitempty"`
+	StripePlan  string             `json:"stripe_plan,omitempty" bson:"stripe_plan,omitempty"`
+	Quantity    int                `json:"quantity,omitempty" bson:"quantity,omitempty"`
+	TrialEndsAt string             `json:"trial_ends_at,omitempty" bson:"trial_ends_at,omitempty"`
+	EndsAt      string             `json:"ends_at,omitempty" bson:"ends_at,omitempty"`
+	UserId      string             `json:"user_id,omitempty" bson:"user_id,omitempty"`
+	UpdatedAt   string             `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	CreatedAt   string             `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
