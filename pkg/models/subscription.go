8bddcdd73e3d77a4bcf8947dff675802d2e75f31
Cédric Verstraeten <cedric@verstraeten.io>
2026-01-26 07:54:22 +0000

    Refactor Subscription struct to use time.Time for date fields

--- pkg/models/subscription.go
+++ pkg/models/subscription.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -9,9 +11,9 @@ type Subscription struct {
 	StripeId    string             `json:"stripe_id,omitempty" bson:"stripe_id,omitempty"`
 	StripePlan  string             `json:"stripe_plan,omitempty" bson:"stripe_plan,omitempty"`
 	Quantity    int                `json:"quantity,omitempty" bson:"quantity,omitempty"`
-	TrialEndsAt string             `json:"trial_ends_at,omitempty" bson:"trial_ends_at,omitempty"`
-	EndsAt      string             `json:"ends_at,omitempty" bson:"ends_at,omitempty"`
+	TrialEndsAt time.Time          `json:"trial_ends_at,omitempty" bson:"trial_ends_at,omitempty"`
+	EndsAt      time.Time          `json:"ends_at,omitempty" bson:"ends_at,omitempty"`
 	UserId      string             `json:"user_id,omitempty" bson:"user_id,omitempty"`
-	UpdatedAt   string             `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
-	CreatedAt   string             `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt   time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	CreatedAt   time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
